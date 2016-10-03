c3c78a690c7b1b587973faf4a6fc6c7adfb7ee68
cngondo <devngondo@gmail.com>
2016-10-03 12:34:03 +0300

    shifts over to the next activity

--- app/src/main/java/splash/example/com/splashit/SplashActivity.java
+++ app/src/main/java/splash/example/com/splashit/SplashActivity.java
@@ -1,5 +1,6 @@
 package splash.example.com.splashit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,18 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        /*
+        * Note here that we are not loading anyview but we are simply taking advantage
+        * of the cold start time in android by just loading the theme. The cold start provides
+        * with just the enough time to load the splash screen.
+        * */
+        /*
+        * Start the activity and clear it from the back stack using flags
+        * */
+        startActivity(new Intent(SplashActivity.this, MainActivity.class)
+        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        //Make sure that you kill this activity after switching over to the main activity
+        finish();
     }
 }
