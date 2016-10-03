c02e846e022ad27fb3a342a79b4c1f2f0b576952
cngondo <devngondo@gmail.com>
2016-10-03 13:22:28 +0300

    modified README and added supplementary documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # splash-it
-The right way of implementing a splash screen in android
+The right way of implementing a splash screen in android. We are taking advantage of the cold start
+while launching the application.
+Not inflating any view in the splashscreen activity
+
+### Technologies used
+Themes
+Intents
+
+### Source
+
+[Splash screens the right way](https://www.bignerdranch.com/blog/splash-screens-the-right-way/)
--- app/src/main/java/splash/example/com/splashit/SplashActivity.java
+++ app/src/main/java/splash/example/com/splashit/SplashActivity.java
@@ -1,6 +1,7 @@
 package splash.example.com.splashit;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*
-        * Note here that we are not loading anyview but we are simply taking advantage
+        * Note here that we are not loading any view but we are simply taking advantage
         * of the cold start time in android by just loading the theme. The cold start provides
         * with just the enough time to load the splash screen.
         * */
@@ -18,9 +19,12 @@ public class SplashActivity extends AppCompatActivity {
         * Start the activity and clear it from the back stack using flags
         * */
         startActivity(new Intent(SplashActivity.this, MainActivity.class)
-        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-        //Make sure that you kill this activity after switching over to the main activity
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        /*
+        * Make sure that you kill this activity after switching over to the main activity
+        * so that when you press the back button it doesn't reload the splash screen again
+        * */
         finish();
     }
 }
